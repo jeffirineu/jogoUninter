1efbfaf26bd4c95c9be76346013e61b754c47244
jeff <jeffirineu@gmail.com>
2026-03-07 18:53:44 -0300

    Primeiro git

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame as pg
+
+pg.init()
+window = pg.display.set_mode((600, 480))
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            quit()
